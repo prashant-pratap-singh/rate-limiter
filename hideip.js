3e0188beed3c43a2bc8f8d4a5b66c85ebfb18668
Prashant Pratap Singh <prashantpratapsingh2004@gmail.com>
2026-09-25 16:37:28 +0530

    removed duplicate file and proceeding to v2

--- hideip.js
+++
@@ -1,6 +0,0 @@
-exports.hideip=(ip)=>{
-    const octets=ip.split(".");
-    if(octets.length!==4)return ip; //for only ip4
-
-    return `${octets[0]}.***.***.***`;
-}
--- index.js
+++ index.js
@@ -5,22 +5,22 @@ const {hideip}=require("./helpers/hideip");
 const MAX_ALLOWED_REQ=5;
 const MAX_TIME=30_000;
 
-const ip_mapping={};
+// const ip_mapping={};
 
-setInterval(()=>{
-    ip_mapping={};
-    console.log('resetting ip mapping');
-},MAX_TIME);
+// setInterval(()=>{
+//     ip_mapping={};
+//     console.log('resetting ip mapping');
+// },MAX_TIME);
 
 app.use((req,res,next)=>{
     const my_ip=hideip(ip.address());
 
-    ip_mapping[my_ip]=ip_mapping[my_ip]+1 ||1;
-    console.log(`received request no ${ip_mapping[my_ip]}from ${my_ip}`);
-    if(ip_mapping[my_ip]>MAX_ALLOWED_REQ){
-        console.error('too many requests');
-        return res.status(429).send('too many request');
-    }
+    // ip_mapping[my_ip]=ip_mapping[my_ip]+1 ||1;
+    // console.log(`received request no ${ip_mapping[my_ip]}from ${my_ip}`);
+    // if(ip_mapping[my_ip]>MAX_ALLOWED_REQ){
+    //     console.error('too many requests');
+    //     return res.status(429).send('too many request');
+    // }
     next();
 });
 
